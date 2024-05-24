dda10cf2cb7b85b266d62eaf9b0591f3e0512149
Taha Al Akab <alakabtaha@gmail.com>
2024-05-24 02:48:32 +0100

    Cleaning up code and allowing for websocket to set the intial state

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,26 +5,28 @@ import useWebSocket from 'react-use-websocket';
 import { Worker, Services } from './utils/types';
 
 function App() {
+	const [services, setServices] = useState<Services>({} as Services);
+	const [workers, setWorkers] = useState<Worker>({} as Worker);
+	const [region, setRegion] = useState<string>("");
 	const { sendMessage, lastMessage } = useWebSocket('ws://localhost:8000', {
 		onOpen: () => console.log('opened'),
 		onClose: () => console.log('closed'),
 	});
-	const [services, setServices] = useState<Services>({} as Services);
-	const [workers, setWorkers] = useState<Worker>({} as Worker);
-	const [region, setRegion] = useState<string>('');
+	
 
 	useEffect(() => {
 		if (lastMessage !== null) {
 			const data = JSON.parse(lastMessage.data);
-			console.log(data);
 			setServices(data.services);
 			setWorkers(data.worker);
-			console.log(services, workers)
+			if (data.services.region !== region) {
+				setRegion(data.services.region);
+			}
 		}
 	}, [lastMessage]);
 
 	useEffect(() => {
-		sendMessage(region);
+		if (region !== "") sendMessage(region);
 	}, [region]);
 
 	return (
