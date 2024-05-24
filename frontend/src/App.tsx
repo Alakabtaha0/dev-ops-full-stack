7d2f1d5e292343c00d7778e76a1f4af088e1f8ea
Taha Al Akab <alakabtaha@gmail.com>
2024-05-24 02:37:04 +0100

    Exporting types to antoher file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,16 @@ import React, { useEffect, useState } from 'react';
 import "./styles/master.css";
 import NavBar from './components/NavBar';
 import useWebSocket from 'react-use-websocket';
+import { Worker, Services } from './utils/types';
 
 function App() {
 	const { sendMessage, lastMessage } = useWebSocket('ws://localhost:8000', {
 		onOpen: () => console.log('opened'),
 		onClose: () => console.log('closed'),
 	});
-	const [services, setServices] = useState<[]>([]);
-	const [workers, setWorkers] = useState<[]>([]);
-	const [region, setRegion] = useState<string>('us-east');
+	const [services, setServices] = useState<Services>({} as Services);
+	const [workers, setWorkers] = useState<Worker>({} as Worker);
+	const [region, setRegion] = useState<string>('');
 
 	useEffect(() => {
 		if (lastMessage !== null) {
--- frontend/src/utils/types.tsx
+++ frontend/src/utils/types.tsx
@@ -1,5 +1,49 @@
 export interface NavBarProps {
     currentRegion: string;
     setCurrentRegion: React.Dispatch<React.SetStateAction<string>>;
-}
+};
 
+// Worker types
+export type BlockedKeys = {
+    id: string;
+    numberOfTimesBlocked: number;
+    date: string;
+};
+
+export type TopKeys = {
+    id: string;
+    floatNumber: number;
+};
+
+export type WorkerInfo = {
+    idle: string;
+    recenetlyBlockedKeys: BlockedKeys[];
+    timeToReturn: number;
+    topKeys: TopKeys[];
+    waitTime: number;
+    waiting: number;
+    workers: number;
+};
+
+export interface Worker {
+    name: string;
+    worker: WorkerInfo;
+};
+
+
+// Service types
+export type Services = {
+    activeConnections: number;
+    cpuLoad: number;
+    database: boolean;
+    online: number;
+    redis: boolean;
+    region: string;
+    serverCount: number;
+    serverIssue: any | null;
+    session: number;
+    status: string;
+    strict: boolean;
+    timers: number;
+    waitTime: number;
+}
